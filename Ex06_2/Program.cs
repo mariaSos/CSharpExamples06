921a756627a54b3b6824e7c8e9040f6c853bb0a7
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-06-30 22:13:39 +0500

    Задача 43

--- Ex06_2/Program.cs
+++ Ex06_2/Program.cs
@@ -1,9 +1,8 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
 //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
- void Koordinate(int b1, int k1, int b2, int k2, out double y1, out double y2){
-     double x = (b2-b1)/(k1-k2);
-     y1 = k1 * x + b1;
-     y2 = k2 * x + b2;
+ void Koordinate(int b1, int k1, int b2, int k2, out double x, out double y){
+     x = (b2-b1)/(k1-k2);
+     y = k1 * x + b1;
  }
  
  //Ввод чисел
@@ -14,7 +13,6 @@ int[] ReadInt(string text)
 }
 
 int[] nums = ReadInt("Введите k1, b1, k2, b2 через пробел: ");
-//int x = 0;
-//int y = 0;
-Koordinate(nums[0],nums[1],nums[2],nums[3],out double y1, out double y2);
-System.Console.WriteLine($"Координаты точки пересечения двух прямых({y1},{y2})");
+
+Koordinate(nums[0],nums[1],nums[2],nums[3],out double x, out double y);
+System.Console.WriteLine($"x = {x}, y = {y})");
