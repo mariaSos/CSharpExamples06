57ae7732e14c6c319af531189cac2fecdc04609f
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-06-30 21:30:04 +0500

    Задача 41

---
+++ Ex06_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ввод чисел
+int[] ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+
+//Подсчет количества положительных чисел
+int Counter(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+//Выполнение задачи
+int[] array = ReadInt("Введите числа через пробел: ");
+
+System.Console.WriteLine($"Количество положительных чисел равно {Counter(array)}");
